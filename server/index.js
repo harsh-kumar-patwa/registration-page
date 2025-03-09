2fb327cacca46c28f06fed339634b9cf69460905
Harsh Kumar <harshkumar3446@gmail.com>
2025-03-10 00:49:55 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 const db = process.env.MONGODB_URI;
 
 // Middleware
-app.use(cors()); 
+app.use(cors(
+  {
+    origin: 'https://registration-page-finacplus.vercel.app/' 
+  }
+)); 
 app.use(express.json()); 
 
 // Connect to MongoDB
