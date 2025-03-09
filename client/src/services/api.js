d3d8f7d59221ee339e4718b84973c71465576204
Harsh Kumar <harshkumar3446@gmail.com>
2025-03-10 00:15:10 +0530

    upadted cors

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,23 @@ const API_BASE_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 });
+
+// Add request interceptor
+api.interceptors.request.use(
+    (config) => {
+        config.headers['Access-Control-Allow-Origin'] = '*';
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export const createUser = (userData) => api.post('/users', userData);
 export const getUsers = () => api.get('/users');
